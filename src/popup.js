f79cf21907cab051c5b2807a5251777797e5333a
chima <chimaemekaiheonu@gmail.com>
2023-12-25 08:18:45 +0100

    Update popup.html and popup.js files

--- src/popup.js
+++ src/popup.js
@@ -2,38 +2,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Popup = ({ onBlock }) => {
-  const [urls, setUrls] = useState('');
-  const [duration, setDuration] = useState('');
-
-  const handleBlock = () => {
-    const urlList = urls.split(',').map(url => url.trim());
-    const blockingDuration = parseInt(duration, 10);
-
-    if (urlList.length > 0 && blockingDuration > 0) {
-      onBlock(urlList, blockingDuration);
-    }
-  };
-
-  return (
-    <div>
-      <h2>Block Websites</h2>
-      <textarea
-        placeholder="Enter URLs separated by commas"
-        value={urls}
-        onChange={(e) => setUrls(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Enter blocking duration (in minutes)"
-        value={duration}
-        onChange={(e) => setDuration(e.target.value)}
-      />
-      <button onClick={handleBlock}>Block Websites</button>
-    </div>
-  );
-};
-
 // Find the root element in the HTML and render the Popup component inside it
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Popup />, rootElement);
