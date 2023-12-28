fb864a87d8ad6c9313b75d43f77b3390a6cf68cf
chima <chimaemekaiheonu@gmail.com>
2023-12-28 14:45:53 +0100

    final changes

--- src/components/PopupComponent.js
+++ src/components/PopupComponent.js
@@ -1,12 +1,28 @@
 // src/components/PopupComponent.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../PopupComponent.css'; // Import your custom styles
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faInfoCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faPlus, faClock } from '@fortawesome/free-solid-svg-icons';
 
 const PopupComponent = ({ onBlock }) => {
   const [urls, setUrls] = useState('');
   const [duration, setDuration] = useState('');
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 
   const handleBlock = () => {
     const urlList = urls.split(',').map(url => url.trim());
@@ -26,53 +42,37 @@ const PopupComponent = ({ onBlock }) => {
 
   return (
     <div className="custom-popup">
-      <nav className="custom-navbar">
-        <div className="custom-brand">
-          <a href="#">Focus Mode</a>
-        </div>
-        <ul className="custom-icons">
-          <li>
-            <a href="#" title="Home">
-              <FontAwesomeIcon icon={faHome} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Information">
-              <FontAwesomeIcon icon={faInfoCircle} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Contact">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className="info-icon">
+        <FontAwesomeIcon icon={faInfoCircle} />
+      </div>
+      <div className="pink-container"> {/* Added pink-container div */}
+        <h3 className="focus-mode">Focus Mode</h3>
+      </div>
+      <div className='time'>{currentTime}</div>       
       <div className="custom-content">
-        <h2 className="mb-4">Block Websites</h2>
-        <div className="mb-3">
-          <textarea
-            className="custom-textarea"
-            rows="4"
-            placeholder="Enter URLs separated by commas"
-            value={urls}
-            onChange={(e) => setUrls(e.target.value)}
-          />
-        </div>
+        <h2>Block Websites</h2>
         <div className="mb-3">
           <input
             type="number"
-            className="custom-input"
             placeholder="Enter blocking duration (in minutes)"
             value={duration}
             onChange={(e) => setDuration(e.target.value)}
           />
         </div>
-        <button className="custom-btn" onClick={handleBlock}>
-          Block Websites
-        </button>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Add new site"
+            value={urls}
+            onChange={(e) => setUrls(e.target.value)}
+          />
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
       </div>
-      <footer className="custom-footer mt-4">
+      <button className="btn block-btn" onClick={handleBlock}>
+         Enter Focus Mode
+      </button>
+      <footer className="custom-footer">
         <p>&copy; 2023 Focus Mode</p>
       </footer>
     </div>
